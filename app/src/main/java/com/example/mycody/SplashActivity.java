7b5238359d09acf65549388d36ad0772a5235c5e
kmh339 <kmh339@gmail.com>
2019-11-20 15:34:43 +0900

    Initial commit

---
+++ app/src/main/java/com/example/mycody/SplashActivity.java
@@ -0,0 +1,21 @@
+package com.example.mycody;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+
+        try{
+            Thread.sleep(2000);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+    }
+}
