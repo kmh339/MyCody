9f1da6109f0c4abaf416d8378a3642b39581f25c
kmh339 <kmh339@gmail.com>
2019-11-23 13:48:52 +0900

    SQLite 2nd Try
    Recycler View Try2

--- app/src/main/java/com/example/mycody/TopCody.java
+++ app/src/main/java/com/example/mycody/TopCody.java
@@ -1,10 +1,14 @@
 package com.example.mycody;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class TopCody extends AppCompatActivity {
+    Button camera_btn;
+    Button gallery_btn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
